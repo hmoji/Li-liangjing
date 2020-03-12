4cb1ecfd309099a010b98e11f5393074bd3ac945
hmoji <945619928@qq.com>
2020-03-12 17:48:57 +0800

    Revert "未完成"
    This reverts commit 695d571acdef80aff4b91afb4a3742a177bd186b.

--- Li liangjing/C综合训练---字符串替换.cpp
+++ Li liangjing/C综合训练---字符串替换.cpp
@@ -2,7 +2,7 @@
 #include<stdio.h>
 #include<string.h>
 int main() {
-	FILE *file1,*work;
+	FILE *file1;
 	FILE *file2;
 	file1 = fopen("filein.txt", "r");
 	if (file1 == NULL) {
@@ -13,32 +13,5 @@ int main() {
 	char ori[10], rep[10];
 	scanf("%s", ori);
 	scanf("%s", rep);
-	char n;
-	int flag=0;
-	while (file1 != NULL) {
-		n = fgetc(file1);
-		if (n == ori[0]) {
-			work = file1;
-			for (int i = 1; i < strlen(ori); i++) {
-				if (fgetc(work) != ori[i]) {
-					flag = 1;
-				}
-			}
-		}
-		else {
-			flag = 1;
-		}
-		if (flag == 1) {
-			fputc(n, file2);
-		}
-		else {
-			for (int i = 0; i < strlen(rep); i++) {
-				fputc(rep[i], file2);
-			}
-			fseek(file1, strlen(ori) - 1, 1);
-		}
-		flag = 0;
-	}
-	fclose(file1);
-	fclose(file2);
+
 }
